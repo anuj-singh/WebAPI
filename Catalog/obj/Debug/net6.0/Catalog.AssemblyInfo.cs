548886da714e51e19e59d10bb55c9969b90d9e4a
Anuj Singh <anujk.singh@outlook.com>
2023-12-31 22:17:19 +0530

    DBAuth enabled with Dotnet secrets

--- Catalog/obj/Debug/net6.0/Catalog.AssemblyInfo.cs
+++ Catalog/obj/Debug/net6.0/Catalog.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("f3360cea-4e2f-4f6d-8d98-da9159dcb3f2")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
